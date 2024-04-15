72e6625695eabd7034d220a3348c4565eb22a26f
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-04-15 10:50:52 +0000

    upadated todo

--- todo_backened/index.js
+++ todo_backened/index.js
@@ -1,161 +1,161 @@
 const express = require('express');
-  const bodyParser = require('body-parser');
-  const fs = require('fs');
-  const app = express();
-  const port = process.env.PORT // || 8000;
-  const path = require('path');
-  var val =  0;
-  
-  app.use(bodyParser.json());
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const port = process.env.PORT // || 8000;
+const path = require('path');
+var val = 0;
+
+app.use(bodyParser.json());
 
 
-  class ToDo{
-    constructor(title,status, description){
-      this.id = ++val;
-      this.title = title;
-      this.status = status;
-      this.description = description;
-    }
+class ToDo {
+  constructor(title, status, description) {
+    this.id = ++val;
+    this.title = title;
+    this.status = status;
+    this.description = description;
   }
-  
-  var array = [];
-  function addToDo(todo){
-    array.push(todo);
-  }
-  function getToDos(){
-      return array;
-  }
-  
+}
 
-  function getId(){
+var array = [];
+function addToDo(todo) {
+  array.push(todo);
+}
+function getToDos() {
+  return array;
+}
+
+
+function getId() {
   //  loadTodosFromFile();
-    const maxId = array.reduce(function(max, todo) {
-      return Math.max(max, todo.id);
-    }, 0);
-  
+  const maxId = array.reduce(function (max, todo) {
+    return Math.max(max, todo.id);
+  }, 0);
+
   // Set val to the maximum id
-    val = maxId;
-  }
-  function find(id){
+  val = maxId;
+}
+function find(id) {
 
- //   loadTodosFromFile()
-    for(var i=0; i<array.length; i++){
-      if(array[i].id == id){
-        return i;
-      }
-    }return -1;
-  }
-  function postToDo(request, response){
-    //loadTodosFromFile()
-    getId()
-    var title = request.body.title;
-    var description = request.body.description;
-    var status = request.body.status;
-  
-    var todo = new ToDo(title,status,description)
-    addToDo(todo);
-    
-    response.status(200).json(todo);
-    writeTodosToFile()
-  }
-  
-  function getTodo(req, res){
-    var id = req.params.id;
-    var i = find(id);
-    if(i != -1){
-      res.status(200).json(array[i]);
-    }else{
-      res.status(404).send('Not Found');
+  //   loadTodosFromFile()
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].id == id) {
+      return i;
     }
-  }
-  
-  function allTodos(req, res){
-    
-    res.status(200).json(array);
-  }
-  
-  function putToDo(request, response){
-    var id = request.params.id;
-    var i = find(id);
-    if(i != -1){
-      console.log("putToDo")
-      array[i].status = request.body.status;
-      array[i].description = request.body.description;
-      array[i].title = request.body.title;
-      response.status(200).json(array[i]);
-    }else{
-      response.status(404).send('Not Found');
-    }
-    writeTodosToFile()
-  }
-  
-  function deleteToDo(request, response){
-    var id = request.params.id;
-    var i = find(id);
-    if(i != -1){
-      array.splice(i,1);
-      response.status(200).send('OK');
-    }else{
-      response.status(404).send('Not Found');
-    }
-    writeTodosToFile()
-  }
-  
-  function start(){
-    console.log(`Server is running on port`);
-    loadTodosFromFile()
-  }
+  } return -1;
+}
+function postToDo(request, response) {
+  //loadTodosFromFile()
+  getId()
+  var title = request.body.title;
+  var description = request.body.description;
+  var status = request.body.status;
 
-  function helpRead(err, data){
-    if(err){
-      console.log("Error loading todos : " +err);
-      return
-    }
-    array = JSON.parse(data);
-    console.log("Data loaded from file");
+  var todo = new ToDo(title, status, description)
+  addToDo(todo);
+
+  response.status(200).json(todo);
+  writeTodosToFile()
+}
+
+function getTodo(req, res) {
+  var id = req.params.id;
+  var i = find(id);
+  if (i != -1) {
+    res.status(200).json(array[i]);
+  } else {
+    res.status(404).send('Not Found');
   }
-function loadTodosFromFile(){
+}
+
+function allTodos(req, res) {
+
+  res.status(200).json(array);
+}
+
+function putToDo(request, response) {
+  var id = request.params.id;
+  var i = find(id);
+  if (i != -1) {
+    console.log("putToDo")
+    array[i].status = request.body.status;
+    array[i].description = request.body.description;
+    array[i].title = request.body.title;
+    response.status(200).json(array[i]);
+  } else {
+    response.status(404).send('Not Found');
+  }
+  writeTodosToFile()
+}
+
+function deleteToDo(request, response) {
+  var id = request.params.id;
+  var i = find(id);
+  if (i != -1) {
+    array.splice(i, 1);
+    response.status(200).send('OK');
+  } else {
+    response.status(404).send('Not Found');
+  }
+  writeTodosToFile()
+}
+
+function start() {
+  console.log(`Server is running on port`);
+  loadTodosFromFile()
+}
+
+function helpRead(err, data) {
+  if (err) {
+    console.log("Error loading todos : " + err);
+    return
+  }
+  array = JSON.parse(data);
+  console.log("Data loaded from file");
+}
+function loadTodosFromFile() {
   fs.readFile("data/todo.json", "utf8", helpRead);
 }
 
-function helpWrite(err){
-if(err){
-  console.log("Error saving todos : " +err);
-  return
-}
-console.log("Todos saved ")
+function helpWrite(err) {
+  if (err) {
+    console.log("Error saving todos : " + err);
+    return
+  }
+  console.log("Todos saved ")
 
 }
-function writeTodosToFile(){
+function writeTodosToFile() {
 
   fs.writeFile("data/todo.json", JSON.stringify(array), helpWrite);
 }
 
-  function middleware(req, res,next){
-   // loadTodosFromFile()
-   
+function middleware(req, res, next) {
+  // loadTodosFromFile()
+
   console.log("Setting")
-    next();
-  }
+  next();
+}
 
 //   app.get('/', function(req, res) {
 //     fs.readFile(__dirname + 'index.html', 'utf8', function(err, text){
 //       res.send(text);
 //   })
 // });
-  
 
-  app.use(middleware)
-  app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname + '/index.html'));
-  });
-  
-  //app.get('/', sendFile('index.html'));
-  app.get('/todos', allTodos)
-  app.get('/todos/:id', getTodo)
-  app.post('/todos', postToDo)
-  app.put('/todos/:id', putToDo)
-  app.delete('/todos/:id', deleteToDo)
-  
- app.listen(port, start);
-  module.exports = app;
+
+app.use(middleware)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+//app.get('/', sendFile('index.html'));
+app.get('/todos', allTodos)
+app.get('/todos/:id', getTodo)
+app.post('/todos', postToDo)
+app.put('/todos/:id', putToDo)
+app.delete('/todos/:id', deleteToDo)
+
+app.listen(port, start);
+module.exports = app;
